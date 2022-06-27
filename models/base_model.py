2913362d815f68b366f4f73c1118051dacf013fa
yuan-fang-228 <fy0228@gmail.com>
2022-06-27 09:10:38 -0700

    task 4 update

--- models/base_model.py
+++ models/base_model.py
@@ -13,11 +13,12 @@ class BaseModel():
             initialises a BaseModel instance
         """
         dateformat = "%Y-%m-%dT%H:%M:%S.%f"
-        if kwargs.__len__() != 0:
+        if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     self.__dict__[key] = datetime.strptime(value, dateformat)
-                self.__dict__[key] = value
+                else:
+                    self.__dict__[key] = value
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
