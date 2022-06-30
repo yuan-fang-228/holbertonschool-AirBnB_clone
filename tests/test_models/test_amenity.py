38d940eaf9b33d620c27c5b6d5e64be0df076d8e
yuan-fang-228 <fy0228@gmail.com>
2022-06-30 00:10:00 -0700

    added unittest for amenity

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,1 +1,84 @@
 #!/usr/bin/python3
+""" Unit test for Amenity """
+
+import unittest
+import pycodestyle
+import json
+import os
+import inspect
+import datetime
+from uuid import uuid4
+
+from models.amenity import Amenity
+from models.base_model import BaseModel
+
+
+class TestDocs(unittest.TestCase):
+    """ tests for docs and style """
+
+    @classmethod
+    def setUpClass(cls):
+        """ setup for function doc tests"""
+        cls.base_funcs = inspect.getmembers(Amenity, inspect.isfunction)
+
+    def test_class_style(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['models/amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_test_style(self):
+        """Test that we conform to Pycodestyle."""
+        style = pycodestyle.StyleGuide(quiet=True)
+        result = style.check_files(['tests/test_models/test_amenity.py'])
+        self.assertEqual(result.total_errors, 0,
+                         "Found code style errors (and warnings).")
+
+    def test_class_doc(self):
+        """ Tests for class docs """
+        self.assertTrue(len(Amenity.__doc__) >= 1)
+
+    def test_func_doc(self):
+        """Tests for docstrings in all functions """
+        for func in self.base_funcs:
+            self.assertTrue(len(func[1].__doc__) >= 1)
+
+
+class TestAmenity(unittest.TestCase):
+    """ tests for class functions and attributes """
+
+    def setUp(self):
+        """ initialising for tests """
+        self.am = Amenity()
+        self.am1 = Amenity()
+        self.am1.name = "gym"
+
+    def test_class_type(self):
+        """ test if Amenity inherits from BaseModel """
+        self.assertEqual(issubclass(Amenity, BaseModel), True)
+
+    def test_instance_type(self):
+        """test if amenity is a subclass of basemodel"""
+        self.assertEqual(type(self.am), Amenity)
+
+    def test_attr_not_none(self):
+        """ test if id, create and update exit """
+        self.assertIsNotNone(self.am.id)
+        self.assertIsNotNone(self.am.created_at)
+        self.assertIsNotNone(self.am.updated_at)
+
+    def test_attr_name_value(self):
+        """ test the attribute name value """
+        self.assertEqual(self.am1.name, "gym")
+        self.assertEqual(self.am.name, "")
+
+    def test_attr_type(self):
+        """ test the type of id, created_at and updated_at """
+        self.assertEqual(type(self.am.id), str)
+        self.assertEqual(type(self.am.created_at), datetime.datetime)
+        self.assertEqual(type(self.am.updated_at), datetime.datetime)
+
+
+if __name__ == "__main__":
+    unitest.main()
