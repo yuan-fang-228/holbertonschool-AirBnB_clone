a8a04aa17ec276a90bc94e4150ebac0bf981bc1b
yuan-fang-228 <fy0228@gmail.com>
2022-07-01 21:15:11 -0700

    test

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
-"""
-Class BaseModel
-"""
 from uuid import uuid4
 from datetime import datetime
 import models
@@ -9,48 +6,49 @@ import models
 
 class BaseModel:
     """
-    Base class for the Airbnb project
+    Base class
     """
     def __init__(self, *args, **kwargs):
         """
-        Initialises a BaseModel instance
+        Initialize an instance of BaseModel
         """
-        dateformat = "%Y-%m-%dT%H:%M:%S.%f"
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
-                    self.__dict__[key] = datetime.strptime(value, dateformat)
+                    dttime_ob = datetime.strptime(value,
+                                                  '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, dttime_ob)
                 elif key != "__class__":
-                    self.__dict__[key] = value
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
-            models.storage.save()
 
     def __str__(self):
         """
-        Returns the string of a BaseModel instance
+        Return a string representation of BaseModel
         """
-        return (f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
+        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
         """
-        Changes the updated_at time of an instance
+        Updates the updated_at public instance attribute with the current
+        datetime
         """
         self.updated_at = datetime.now()
         models.storage.save()
 
     def to_dict(self):
         """
-        Returns a dictionary containing all keys/values of __dict__
+        Return a dictionary containing all key/values of __dict__
         """
-        my_dict = {}
-        my_dict["__class__"] = self.__class__.__name__
-        for key, value in self.__dict__.items():
-            if type(value) is datetime:
-                my_dict[key] = value.isoformat()
+        dic = {}
+        dic["__class__"] = self.__class__.__name__
+        for k, v in self.__dict__.items():
+            if isinstance(v, datetime):
+                dic[k] = v.isoformat()
             else:
-                my_dict[key] = value
-        return my_dict
+                dic[k] = v
+        return dic
